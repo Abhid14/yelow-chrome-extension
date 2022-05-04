2c25c9b2ae5d22a95ece6c8c3e768cf816cfc2c3
Abhishek Das <abhid14@outlook.com>
2022-05-04 09:23:54 +0530

    commit

--- yelow-shopping/pages/index.tsx
+++ yelow-shopping/pages/index.tsx
@@ -15,9 +15,22 @@ import {
   Spacer,
   Tag,
   useColorModeValue,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
+import React from "react";
 import { Icon } from "@iconify/react";
-export default function HoePage() {
+
+export default function HomePage() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [placement, setPlacement] = React.useState("left");
+  const btnRef = React.useRef();
   return (
     <Box
       h={575}
@@ -26,35 +39,76 @@ export default function HoePage() {
       //bg={useColorModeValue('blue.100', 'gray.900')}
       //color={useColorModeValue('gray.700', 'gray.200')}
     >
+      <Drawer
+        h={575}
+        placement={placement}
+        size={"x"}
+        onClose={onClose}
+        isOpen={isOpen}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <Flex
+            h={"575"}
+            w={"69px"}
+            justifyContent={"space-between"}
+            flexDirection={"column"}
+          >
+            <Flex mt={5} gap={9} flexDirection={"column"} alignItems={"center"}>
+              <Icon
+                ref={btnRef}
+                onClick={onOpen}
+                height={22}
+                icon="charm:menu-hamburger"
+              />
+              <Icon height={22} icon="akar-icons:cart" />
+              <Icon height={22} icon="lucide:package-search" />
+              <Icon height={22} icon="akar-icons:heart" />
+            </Flex>
+            <Flex mb={5} gap={9} flexDirection={"column"} alignItems={"center"}>
+              <Icon height={22} icon="bi:person" />
+              <Icon height={22} icon="ic:round-logout" />
+            </Flex>
+          </Flex>
+        </DrawerContent>
+      </Drawer>
       <Flex flexDirection={"row"}>
         <Flex
-          h={574}
-          w={50}
+          h={"575"}
+          w={"69px"}
           border={"1px"}
           justifyContent={"space-between"}
           flexDirection={"column"}
         >
-          <Flex mt={5} gap={8} flexDirection={"column"} alignItems={"center"}>
-          <Icon icon="charm:menu-hamburger" />
-            <Icon icon="akar-icons:cart" />
-            <Icon icon="lucide:package-search" />
-            <Icon icon="akar-icons:heart" />
+          <Flex mt={5} gap={9} flexDirection={"column"} alignItems={"center"}>
+            <Icon
+              ref={btnRef}
+              onClick={onOpen}
+              height={22}
+              icon="charm:menu-hamburger"
+            />
+            <Icon height={22} icon="akar-icons:cart" />
+            <Icon height={22} icon="lucide:package-search" />
+            <Icon height={22} icon="akar-icons:heart" />
           </Flex>
-          <Flex mb={5} gap={8} flexDirection={"column"} alignItems={"center"}>
-            <div>1</div>
-            <div>2</div>
+          <Flex mb={5} gap={9} flexDirection={"column"} alignItems={"center"}>
+            <Icon height={22} icon="bi:person" />
+            <Icon height={22} icon="ic:round-logout" />
           </Flex>
         </Flex>
-        <Flex flexDirection={"column"} alignItems={"center"}>
+        <Flex w={"full"} mt={"22.5px"} ml={4} mr={7} flexDirection={"column"}>
           <Flex
-            border={"1px"}
-            h={50}
+            className="rItems"
             alignItems={"center"}
             justifyContent={"space-between"}
             flexDirection={"row"}
           >
-            <Image h={8} src="/images/logo.png"></Image>
+            <Image h={5} src="/images/logo.png"></Image>
+            <Icon height={22} icon="bytesize:close" />
           </Flex>
+          <Text fontSize={17} ml={0.9} mt={8}>
+            My Orders
+          </Text>
         </Flex>
       </Flex>
     </Box>
